cf423280947547f9b349e838caed21414899433c
chriserl <chriserldev@gmail.com>
2020-12-16 12:51:29 +0000

    setup: configured project to use typescript Changed .js files to .tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>Base erlgram app</main>
+		</div>
+	);
+}
